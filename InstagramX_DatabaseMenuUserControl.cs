890a563decebee3bd506b98e8889549c8babb495
Mert Gunduz <merttgg@gmail.com>
2021-09-28 17:10:21 +0300

    User Controls Added

---
+++ InstagramX_DatabaseMenuUserControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InstagramX
+{
+    public partial class InstagramX_DatabaseMenuUserControl : UserControl
+    {
+        public InstagramX_DatabaseMenuUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ InstagramX_StatisticsMenuUserControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InstagramX
+{
+    public partial class InstagramX_StatisticsMenuUserControl : UserControl
+    {
+        public InstagramX_StatisticsMenuUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
