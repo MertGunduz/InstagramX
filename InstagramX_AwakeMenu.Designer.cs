57561c3a51fff4c2fd71567df37a6d70f20342b0
Mert Gunduz <merttgg@gmail.com>
2021-09-28 10:11:09 +0300

    Awake Menu UI Finish

--- InstagramX_AwakeMenu.Designer.cs
+++ InstagramX_AwakeMenu.Designer.cs
@@ -29,22 +29,282 @@ namespace InstagramX
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InstagramX_AwakeMenu));
+            this.Main_Panel = new System.Windows.Forms.Panel();
+            this.TopBorder_Panel = new System.Windows.Forms.Panel();
+            this.BottomBorder_Panel = new System.Windows.Forms.Panel();
+            this.RightBorder_Panel = new System.Windows.Forms.Panel();
+            this.LeftBorder_Panel = new System.Windows.Forms.Panel();
+            this.TopMargin_Panel = new System.Windows.Forms.Panel();
+            this.BottomMargin_Panel = new System.Windows.Forms.Panel();
+            this.RightMargin_Panel = new System.Windows.Forms.Panel();
+            this.LeftMargin_Panel = new System.Windows.Forms.Panel();
+            this.InTopBorder_Panel = new System.Windows.Forms.Panel();
+            this.InBottomBorder_Panel = new System.Windows.Forms.Panel();
+            this.InRightBorder_Panel = new System.Windows.Forms.Panel();
+            this.InLeftBorder_Panel = new System.Windows.Forms.Panel();
+            this.MainLogo_Panel = new System.Windows.Forms.Panel();
+            this.InBottomMargin_Panel = new System.Windows.Forms.Panel();
+            this.InTopMargin_Panel = new System.Windows.Forms.Panel();
+            this.InRightMargin_Panel = new System.Windows.Forms.Panel();
+            this.InLeftMargin_Panel = new System.Windows.Forms.Panel();
+            this.MainLogoHeader_Panel = new System.Windows.Forms.Panel();
+            this.LabelLogo_PictureBox = new System.Windows.Forms.PictureBox();
+            this.Logo_PictureBox = new System.Windows.Forms.PictureBox();
+            this.Main_Panel.SuspendLayout();
+            this.MainLogo_Panel.SuspendLayout();
+            this.MainLogoHeader_Panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LabelLogo_PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo_PictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Main_Panel
+            // 
+            this.Main_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.Main_Panel.Controls.Add(this.MainLogo_Panel);
+            this.Main_Panel.Controls.Add(this.InLeftBorder_Panel);
+            this.Main_Panel.Controls.Add(this.InRightBorder_Panel);
+            this.Main_Panel.Controls.Add(this.InBottomBorder_Panel);
+            this.Main_Panel.Controls.Add(this.InTopBorder_Panel);
+            this.Main_Panel.Controls.Add(this.LeftMargin_Panel);
+            this.Main_Panel.Controls.Add(this.RightMargin_Panel);
+            this.Main_Panel.Controls.Add(this.BottomMargin_Panel);
+            this.Main_Panel.Controls.Add(this.TopMargin_Panel);
+            this.Main_Panel.Controls.Add(this.LeftBorder_Panel);
+            this.Main_Panel.Controls.Add(this.RightBorder_Panel);
+            this.Main_Panel.Controls.Add(this.BottomBorder_Panel);
+            this.Main_Panel.Controls.Add(this.TopBorder_Panel);
+            this.Main_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Main_Panel.Location = new System.Drawing.Point(0, 0);
+            this.Main_Panel.Name = "Main_Panel";
+            this.Main_Panel.Size = new System.Drawing.Size(400, 400);
+            this.Main_Panel.TabIndex = 0;
+            // 
+            // TopBorder_Panel
+            // 
+            this.TopBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.TopBorder_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopBorder_Panel.Location = new System.Drawing.Point(0, 0);
+            this.TopBorder_Panel.Name = "TopBorder_Panel";
+            this.TopBorder_Panel.Size = new System.Drawing.Size(400, 2);
+            this.TopBorder_Panel.TabIndex = 0;
+            // 
+            // BottomBorder_Panel
+            // 
+            this.BottomBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.BottomBorder_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomBorder_Panel.Location = new System.Drawing.Point(0, 398);
+            this.BottomBorder_Panel.Name = "BottomBorder_Panel";
+            this.BottomBorder_Panel.Size = new System.Drawing.Size(400, 2);
+            this.BottomBorder_Panel.TabIndex = 1;
+            // 
+            // RightBorder_Panel
+            // 
+            this.RightBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.RightBorder_Panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.RightBorder_Panel.Location = new System.Drawing.Point(398, 2);
+            this.RightBorder_Panel.Name = "RightBorder_Panel";
+            this.RightBorder_Panel.Size = new System.Drawing.Size(2, 396);
+            this.RightBorder_Panel.TabIndex = 2;
+            // 
+            // LeftBorder_Panel
+            // 
+            this.LeftBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.LeftBorder_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftBorder_Panel.Location = new System.Drawing.Point(0, 2);
+            this.LeftBorder_Panel.Name = "LeftBorder_Panel";
+            this.LeftBorder_Panel.Size = new System.Drawing.Size(2, 396);
+            this.LeftBorder_Panel.TabIndex = 3;
+            // 
+            // TopMargin_Panel
+            // 
+            this.TopMargin_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopMargin_Panel.Location = new System.Drawing.Point(2, 2);
+            this.TopMargin_Panel.Name = "TopMargin_Panel";
+            this.TopMargin_Panel.Size = new System.Drawing.Size(396, 50);
+            this.TopMargin_Panel.TabIndex = 4;
+            // 
+            // BottomMargin_Panel
+            // 
+            this.BottomMargin_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomMargin_Panel.Location = new System.Drawing.Point(2, 348);
+            this.BottomMargin_Panel.Name = "BottomMargin_Panel";
+            this.BottomMargin_Panel.Size = new System.Drawing.Size(396, 50);
+            this.BottomMargin_Panel.TabIndex = 5;
+            // 
+            // RightMargin_Panel
+            // 
+            this.RightMargin_Panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.RightMargin_Panel.Location = new System.Drawing.Point(348, 52);
+            this.RightMargin_Panel.Name = "RightMargin_Panel";
+            this.RightMargin_Panel.Size = new System.Drawing.Size(50, 296);
+            this.RightMargin_Panel.TabIndex = 6;
+            // 
+            // LeftMargin_Panel
+            // 
+            this.LeftMargin_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftMargin_Panel.Location = new System.Drawing.Point(2, 52);
+            this.LeftMargin_Panel.Name = "LeftMargin_Panel";
+            this.LeftMargin_Panel.Size = new System.Drawing.Size(50, 296);
+            this.LeftMargin_Panel.TabIndex = 7;
+            // 
+            // InTopBorder_Panel
+            // 
+            this.InTopBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.InTopBorder_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.InTopBorder_Panel.Location = new System.Drawing.Point(52, 52);
+            this.InTopBorder_Panel.Name = "InTopBorder_Panel";
+            this.InTopBorder_Panel.Size = new System.Drawing.Size(296, 2);
+            this.InTopBorder_Panel.TabIndex = 8;
+            // 
+            // InBottomBorder_Panel
+            // 
+            this.InBottomBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.InBottomBorder_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.InBottomBorder_Panel.Location = new System.Drawing.Point(52, 346);
+            this.InBottomBorder_Panel.Name = "InBottomBorder_Panel";
+            this.InBottomBorder_Panel.Size = new System.Drawing.Size(296, 2);
+            this.InBottomBorder_Panel.TabIndex = 9;
+            // 
+            // InRightBorder_Panel
+            // 
+            this.InRightBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.InRightBorder_Panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.InRightBorder_Panel.Location = new System.Drawing.Point(346, 54);
+            this.InRightBorder_Panel.Name = "InRightBorder_Panel";
+            this.InRightBorder_Panel.Size = new System.Drawing.Size(2, 292);
+            this.InRightBorder_Panel.TabIndex = 10;
+            // 
+            // InLeftBorder_Panel
+            // 
+            this.InLeftBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(150)))), ((int)(((byte)(239)))));
+            this.InLeftBorder_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.InLeftBorder_Panel.Location = new System.Drawing.Point(52, 54);
+            this.InLeftBorder_Panel.Name = "InLeftBorder_Panel";
+            this.InLeftBorder_Panel.Size = new System.Drawing.Size(2, 292);
+            this.InLeftBorder_Panel.TabIndex = 11;
+            // 
+            // MainLogo_Panel
+            // 
+            this.MainLogo_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.MainLogo_Panel.Controls.Add(this.MainLogoHeader_Panel);
+            this.MainLogo_Panel.Controls.Add(this.InLeftMargin_Panel);
+            this.MainLogo_Panel.Controls.Add(this.InRightMargin_Panel);
+            this.MainLogo_Panel.Controls.Add(this.InTopMargin_Panel);
+            this.MainLogo_Panel.Controls.Add(this.InBottomMargin_Panel);
+            this.MainLogo_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainLogo_Panel.Location = new System.Drawing.Point(54, 54);
+            this.MainLogo_Panel.Name = "MainLogo_Panel";
+            this.MainLogo_Panel.Size = new System.Drawing.Size(292, 292);
+            this.MainLogo_Panel.TabIndex = 12;
+            // 
+            // InBottomMargin_Panel
+            // 
+            this.InBottomMargin_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.InBottomMargin_Panel.Location = new System.Drawing.Point(0, 252);
+            this.InBottomMargin_Panel.Name = "InBottomMargin_Panel";
+            this.InBottomMargin_Panel.Size = new System.Drawing.Size(292, 40);
+            this.InBottomMargin_Panel.TabIndex = 6;
+            // 
+            // InTopMargin_Panel
+            // 
+            this.InTopMargin_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.InTopMargin_Panel.Location = new System.Drawing.Point(0, 0);
+            this.InTopMargin_Panel.Name = "InTopMargin_Panel";
+            this.InTopMargin_Panel.Size = new System.Drawing.Size(292, 40);
+            this.InTopMargin_Panel.TabIndex = 7;
+            // 
+            // InRightMargin_Panel
+            // 
+            this.InRightMargin_Panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.InRightMargin_Panel.Location = new System.Drawing.Point(252, 40);
+            this.InRightMargin_Panel.Name = "InRightMargin_Panel";
+            this.InRightMargin_Panel.Size = new System.Drawing.Size(40, 212);
+            this.InRightMargin_Panel.TabIndex = 8;
+            // 
+            // InLeftMargin_Panel
+            // 
+            this.InLeftMargin_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.InLeftMargin_Panel.Location = new System.Drawing.Point(0, 40);
+            this.InLeftMargin_Panel.Name = "InLeftMargin_Panel";
+            this.InLeftMargin_Panel.Size = new System.Drawing.Size(40, 212);
+            this.InLeftMargin_Panel.TabIndex = 9;
+            // 
+            // MainLogoHeader_Panel
+            // 
+            this.MainLogoHeader_Panel.Controls.Add(this.LabelLogo_PictureBox);
+            this.MainLogoHeader_Panel.Controls.Add(this.Logo_PictureBox);
+            this.MainLogoHeader_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainLogoHeader_Panel.Location = new System.Drawing.Point(40, 40);
+            this.MainLogoHeader_Panel.Name = "MainLogoHeader_Panel";
+            this.MainLogoHeader_Panel.Size = new System.Drawing.Size(212, 212);
+            this.MainLogoHeader_Panel.TabIndex = 10;
+            // 
+            // LabelLogo_PictureBox
+            // 
+            this.LabelLogo_PictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LabelLogo_PictureBox.Image = global::InstagramX.Properties.Resources.InstagramX_HeaderLabelIcon;
+            this.LabelLogo_PictureBox.Location = new System.Drawing.Point(0, 144);
+            this.LabelLogo_PictureBox.Name = "LabelLogo_PictureBox";
+            this.LabelLogo_PictureBox.Size = new System.Drawing.Size(212, 68);
+            this.LabelLogo_PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.LabelLogo_PictureBox.TabIndex = 1;
+            this.LabelLogo_PictureBox.TabStop = false;
+            // 
+            // Logo_PictureBox
+            // 
+            this.Logo_PictureBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Logo_PictureBox.Image = global::InstagramX.Properties.Resources.InstagramX_LogoIcon;
+            this.Logo_PictureBox.Location = new System.Drawing.Point(0, 0);
+            this.Logo_PictureBox.Name = "Logo_PictureBox";
+            this.Logo_PictureBox.Size = new System.Drawing.Size(212, 144);
+            this.Logo_PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Logo_PictureBox.TabIndex = 0;
+            this.Logo_PictureBox.TabStop = false;
             // 
             // InstagramX_AwakeMenu
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.Main_Panel);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "InstagramX_AwakeMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "InstagramX";
+            this.Main_Panel.ResumeLayout(false);
+            this.MainLogo_Panel.ResumeLayout(false);
+            this.MainLogoHeader_Panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.LabelLogo_PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo_PictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel Main_Panel;
+        private System.Windows.Forms.Panel LeftBorder_Panel;
+        private System.Windows.Forms.Panel RightBorder_Panel;
+        private System.Windows.Forms.Panel BottomBorder_Panel;
+        private System.Windows.Forms.Panel TopBorder_Panel;
+        private System.Windows.Forms.Panel LeftMargin_Panel;
+        private System.Windows.Forms.Panel RightMargin_Panel;
+        private System.Windows.Forms.Panel BottomMargin_Panel;
+        private System.Windows.Forms.Panel TopMargin_Panel;
+        private System.Windows.Forms.Panel InLeftBorder_Panel;
+        private System.Windows.Forms.Panel InRightBorder_Panel;
+        private System.Windows.Forms.Panel InBottomBorder_Panel;
+        private System.Windows.Forms.Panel InTopBorder_Panel;
+        private System.Windows.Forms.Panel MainLogo_Panel;
+        private System.Windows.Forms.Panel InLeftMargin_Panel;
+        private System.Windows.Forms.Panel InRightMargin_Panel;
+        private System.Windows.Forms.Panel InTopMargin_Panel;
+        private System.Windows.Forms.Panel InBottomMargin_Panel;
+        private System.Windows.Forms.Panel MainLogoHeader_Panel;
+        private System.Windows.Forms.PictureBox LabelLogo_PictureBox;
+        private System.Windows.Forms.PictureBox Logo_PictureBox;
     }
 }
 
