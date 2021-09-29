3eb81ca9d45ff1fb1b6d157bc50955aee79e8a19
Mert Gunduz <merttgg@gmail.com>
2021-09-29 08:59:50 +0300

    Hover Forecolor

--- InstagramX_MainMenuUserControl.cs
+++ InstagramX_MainMenuUserControl.cs
@@ -22,33 +22,39 @@ namespace InstagramX
         private void FollowUser_Button_MouseEnter(object sender, EventArgs e)
         {
             FollowUser_Button.Image = Resources.InstagramX_HoveredFollowerIcon;
+            FollowUser_Button.ForeColor = Color.FromArgb(19, 165, 254);
         }
 
         private void FollowUser_Button_MouseLeave(object sender, EventArgs e)
         {
             FollowUser_Button.Image = Resources.InstagramX_NFollowerIcon;
+            FollowUser_Button.ForeColor = Color.FromArgb(4, 150, 239);
         }
 
         // LikePostsButton (Hover-NonHover)
         private void LikePosts_Button_MouseEnter(object sender, EventArgs e)
         {
             LikePosts_Button.Image = Resources.InstagramX_HoveredLikeIcon;
+            LikePosts_Button.ForeColor = Color.FromArgb(19, 165, 254); 
         }
 
         private void LikePosts_Button_MouseLeave(object sender, EventArgs e)
         {
             LikePosts_Button.Image = Resources.InstagramX_NLikeIcon;
+            LikePosts_Button.ForeColor = Color.FromArgb(4, 150, 239);
         }
 
         // CommentButton (Hover-NonHover)
         private void Comment_Button_MouseEnter(object sender, EventArgs e)
         {
             Comment_Button.Image = Resources.InstagramX_HoveredCommentIcon;
+            Comment_Button.ForeColor = Color.FromArgb(19, 165, 254);
         }
 
         private void Comment_Button_MouseLeave(object sender, EventArgs e)
         {
             Comment_Button.Image = Resources.InstagramX_NCommentIcon;
+            Comment_Button.ForeColor = Color.FromArgb(4, 150, 239);
         }
     }
 }
